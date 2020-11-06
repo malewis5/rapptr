4c72edc717889882617e6559ea315c9234fb1433
Matthew Lewis <malewis5@crimson.ua.edu>
2020-11-06 14:20:53 -0500

    About w/responsive design v1

---
+++ src/components/organisms/About.js
@@ -0,0 +1,26 @@
+import "./About.css";
+
+export const About = () => {
+  return (
+    <div className="About">
+      <div className="About-title">
+        <h2>WHO WE ARE</h2>
+      </div>
+      <div className="About-content">
+        <p>
+          Rapptr Labs is a Jersey City based app development firm that works
+          with Fortune 500 brands, leading retailers, funded startups and moer
+          to craft digital products and strategies that solve business problems
+          and drive measureable results.{" "}
+        </p>
+        <br />
+        <p>
+          We're part of your team. That means working together to meet the
+          business challenges you face. From iOS and Android to emergin
+          technologies like VR, AR, and wearables, we do whatever it takes to
+          help you thrive in today's - and tomorrow's - digital ecosystems.
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -2,11 +2,13 @@ import "./Landing.css";
 
 // Custom Components
 import { Hero } from "../organisms/Hero";
+import { About } from "../organisms/About";
 
 export const Landing = () => {
   return (
     <div className="Landing">
       <Hero />
+      <About />
     </div>
   );
 };
