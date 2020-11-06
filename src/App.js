97f71efa784a770cea98b7b1f27b777834fa27a9
Matthew Lewis <malewis5@crimson.ua.edu>
2020-11-06 12:03:09 -0500

    added assets to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Custom Components
 import { Login } from "./pages/Login";
---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import "./Hero.css";
+import heroImage from "../assets/gpc_hero_image@2x.png";
+
+const Hero = () => {
+  return (
+    <header>
+      <img src={heroImage} alt="Jersey City harbor" />
+    </header>
+  );
+};
