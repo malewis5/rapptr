895e524367528065c0d72551bceb1c1d8dbfb7da
Matthew Lewis <malewis5@crimson.ua.edu>
2020-11-06 13:35:02 -0500

    Hero header v1

--- src/components/organisms/Hero.js
+++ src/components/organisms/Hero.js
@@ -1,9 +1,21 @@
-import heroImage from "../../assets/gpc_hero_image@2x.png";
+import "./Hero.css";
+import { Link } from "react-router-dom";
+
+// Assets
+import logo from "../../assets/logo.png";
 
 export const Hero = () => {
   return (
-    <header>
-      <img src={heroImage} alt="Jersey City harbor" />
-    </header>
+    <div className="Hero">
+      <div className="Hero-login">
+        <Link to="/Login">Login</Link>
+      </div>
+      <div className="Hero-container">
+        <div className="Hero-logo">
+          <img src={logo} alt="Rapptr Logo" />
+          <h2 className="Hero-tagline">APP DESIGN AND DEVELOPMENT AGENCY</h2>
+        </div>
+      </div>
+    </div>
   );
 };
